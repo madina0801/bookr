95a67647bb448aff1b9f73f9cf3cca1479806039
Madina <madinatussupova0820@gmail.com>
2024-03-28 09:44:12 +0500

    render a base template in a view

--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-	name = request.GET.get("name") or "world"
-	return HttpResponse("Hello {}!".format(name))
+	return render(request, 'base.html')
