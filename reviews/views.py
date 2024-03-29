3896930728395182b777a6b857f9a1cd28ea8241
Madina <madinatussupova0820@gmail.com>
2024-03-29 09:38:07 +0500

    using variables at templates

--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-	return render(request, 'base.html')
+	name = "world"
+	return render(request, 'base.html', {"name": name})
