c8ca0a696fa816b8df772d6125d697b8c76d8bf0
Madina <madinatussupova0820@gmail.com>
2024-04-09 13:00:14 +0500

    add str methods to models

--- reviews/models.py
+++ reviews/models.py
@@ -7,6 +7,8 @@ class Publisher(models.Model):
     name = models.CharField(max_length=50, help_text="The name of the Publisher.")
     website = models.URLField(help_text="The Publisher's website.")
     email = models.EmailField(help_text="The Publisher's email address.")
+    def __str__(self):
+        return self.name
 
 
 class Book(models.Model):
@@ -15,16 +17,20 @@ class Book(models.Model):
     isbn = models.CharField(max_length=20, verbose_name="ISBN number of the book.")
     publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
     contributors = models.ManyToManyField("Contributor", through="BookContributor")
+    def __str__(self):
+        return self.title
 
 
 class Contributor(models.Model):
-    firstName = models.CharField(
+    first_names = models.CharField(
         max_length=50, help_text="The contributor's first name or names"
     )
-    lastName = models.CharField(
+    last_names = models.CharField(
         max_length=50, help_text="The contributor's last name or  names"
     )
     email = models.EmailField(help_text="The contact email for the contributor")
+    def __str__(self):
+        return self.first_names
 
 
 class BookContributor(models.Model):
