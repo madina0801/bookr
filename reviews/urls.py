db6c5d9edd042da237c241920e868996260a44c6
Madina <madinatussupova0820@gmail.com>
2024-05-06 12:56:14 +0500

    add books url

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.welcome_view, name='welcome_view'),
+    path("books/", views.book_list, name="book_list"),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,12 +1,35 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Book
-from .utils import average_rating
+from .utils import avg_rating
 
 
 def index(request):
     return render(request, "base.html")
 
 
-def book_search(request):
+def search(request):
     search_text = request.GET.get("search", "")
     return render(request, "reviews/search-results.html", {"search_text": search_text})
+
+
+def book_list(request):
+    books = Book.objects.all()
+    book_list = []
+    for book in books:
+        reviews = book.review_set.all()
+        if reviews:
+            book_rating = avg_rating([review.rating for review in reviews])
+            number_of_reviews = len(reviews)
+        else:
+            book_rating = None
+            number_of_reviews = 0
+        book_list.append(
+            {
+                "book": book,
+                "book_rating": book_rating,
+                "number_of_reviews": number_of_reviews,
+            }
+        )
+
+    context = {"book_list": book_list}
+    return render(request, "reviews/book_list.html", context)
