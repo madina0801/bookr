027b64f7ea03222fb0f868e179dcaa2e55855ac5
Madina <madinatussupova0820@gmail.com>
2024-05-06 12:38:06 +0500

    create book_list view

--- reviews/views.py
+++ reviews/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse 
+from django.shortcuts import render, get_object_or_404
 from .models import Book
+from .utils import average_rating
 
-def welcome_view (request):
-	return render(request, 'base.html')
 
-def search(request):
-	search_result = request.GET.get("search")
-	return render(request, "search.html", {"search": search_result})
+def index(request):
+    return render(request, "base.html")
+
+
+def book_search(request):
+    search_text = request.GET.get("search", "")
+    return render(request, "reviews/search-results.html", {"search_text": search_text})
