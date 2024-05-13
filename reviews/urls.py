46298470761dadb599f21057eb907fde9706d2d6
Madina <madinatussupova0820@gmail.com>
2024-05-13 14:25:30 +0500

    update reviews urls

--- reviews/urls.py
+++ reviews/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="base"),
     path("books/", views.book_list, name="book_list"),
-	path("search-result/", views.search),
-	path('books/<int:pk>/', views.book_detail, name='book_detail')
+		path('books/<int:pk>/', views.book_detail, name='book_detail'),
+		path("search-result/", views.search),
 ]
